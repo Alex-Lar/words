97aa1809ea9e4e272f3c83a9828834afa93ce24b
Alexander Larin <doremilarin@gmail.com>
2024-11-24 17:48:33 +0300

    style: update tab width

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,8 @@
 export interface WordMap {
-	[key: number]: TypedWord[];
+  [key: number]: TypedWord[]
 }
 
 export type TypedWord = {
-	word: string;
-	type: number;
-};
+  word: string
+  type: number
+}
