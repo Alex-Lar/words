96e022b53eebea83f3bded47cc3cbf98d7dca5c4
Alexander Larin <doremilarin@gmail.com>
2024-11-27 22:55:54 +0300

    refactor: move core functions to a separate directory and separate them

---
+++ src/core/sets.ts
@@ -0,0 +1,21 @@
+export function getAllSets() {}
+
+export function getSetSize(key: number) {}
+
+export function getSize() {}
+
+export function containsSet(key: number) {
+  /* Returns true if a set is present in the words object, false otherwise. */
+}
+
+export function getRandomSet() {
+  /* Returns a random set from the words object. */
+}
+
+export function getSet(key: number) {
+  /* Returns the set at a specific index in the words object. */
+}
+
+export function removeSet(setKey: number) {
+  /* Removes a set from the words object. */
+}
---
+++ src/core/wordMap.ts
@@ -0,0 +1,6 @@
+export function registerWordMap() {
+  /* Register external WordMap object */
+}
+export function sortWordsByType() {
+  /* returns a new word map in which words are grouped by their type */
+}
