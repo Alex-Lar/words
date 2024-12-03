29481f5b7ace7f37b1bde0084217708025d67527
Alexander Larin <doremilarin@gmail.com>
2024-12-03 20:51:13 +0300

    chore: remove empty functions

--- src/core/words.ts
+++ src/core/words.ts
@@ -1,10 +1,10 @@
-import { WordsMap, TypedWord, Word } from '@alex-lar/words-shared-types'
+import { WordsMap, Word } from '@alex-lar/words-shared-types'
 
 export function getAllWords(wordsMap: WordsMap): string[] {
   const words = [] as string[]
 
   for (const typedWordArray of Object.values(wordsMap)) {
-    typedWordArray.forEach((word: TypedWord) => words.push(word.name))
+    typedWordArray.forEach((word: Word) => words.push(word.name))
   }
 
   return words
@@ -28,33 +28,4 @@ export function containsWord(wordArray: string[], word: string): boolean {
   if (wordArray.includes(word)) true;
 
   return false
-}
-
-export function containsWordInSet(setKey: number, word: string): boolean {
-  /* Returns true if a word is present in specified setKey, false otherwise. */
-  return false
-}
-
-export function getWordIndex(setKey: number, word: string) {
-  /* Returns the index of a word in a specific set. */
-}
-
-export function getWordAtIndex(setKey: number, index: number) {
-  /* Returns the word at a specific index in a specific set. */
-}
-
-export function getRandomWord() {
-  /* Returns a random word from all sets. */
-}
-
-export function getRandomWordFromSet(setKey: number) {
-  /* Returns a random word from a specific set. */
-}
-
-export function removeWord(setKey: number, word: number) {
-  /* Removes a word from a specific set. */
-}
-
-export function updateWord(setKey: number, oldWord: string, newWord: string) {
-  /* Updates a word in a specific set with a new word. */
 }
