842f0021ea39f4710752b2ba2d06ce8fe64a8464
Alexander Larin <doremilarin@gmail.com>
2024-12-08 13:46:10 +0300

    test: change functions names and add types

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,5 +1,6 @@
-import Words from '../src'
-import { getAllWords } from '../src/core'
+import { Word, WordsMap } from '@alex-lar/words-shared-types'
+import WordsManager from '../src'
+import { getAllNames } from '../src/core'
 import { getIntersection, getUnion } from '../src/core/arrays'
 
 const wm1Array = ['a', 'BB', 'aaa', 'aaaa', 'AAAAA', 'c', 'd']
@@ -7,44 +8,39 @@ const wm1Array = ['a', 'BB', 'aaa', 'aaaa', 'AAAAA', 'c', 'd']
 const wm2Array = ['b', 'BB', 'bbb', 'bbbb', 'AAAAA', 'e', 'f']
 
 const objArr1 = [
-  { name: 'a', type: 0 },
-  { name: 'BB', type: 0 },
-  { name: 'aaa', type: 0 },
-  { name: 'aaaa', type: 0 },
-  { name: 'AAAAA', type: 0 },
-]
+  { name: 'a' },
+  { name: 'BB' },
+  { name: 'aaa' },
+  { name: 'aaaa' },
+  { name: 'AAAAA' },
+] as Word[]
 
 const objArr2 = [
-  { name: 'b', type: 0 },
-  { name: 'BB', type: 0 },
-  { name: 'bbb', type: 0 },
-  { name: 'bbbb', type: 0 },
-  { name: 'AAAAA', type: 0 },
+  { name: 'b' },
+  { name: 'BB' },
+  { name: 'bbb' },
+  { name: 'bbbb' },
+  { name: 'AAAAA' },
 ]
 
 const wm1 = {
-  0: objArr1,
-  1: [
-    { name: 'c', type: 0 },
-    { name: 'd', type: 0 },
-  ],
-}
+  0: objArr1 as Word[],
+  1: [{ name: 'c' }, { name: 'd' }] as Word[],
+} as WordsMap
+
 const wm2 = {
   0: objArr2,
-  1: [
-    { name: 'e', type: 0 },
-    { name: 'f', type: 0 },
-  ],
+  1: [{ name: 'e' }, { name: 'f' }] as Word[],
 }
 
 const unionArray = ['a', 'b', 'BB', 'aaa', 'bbb', 'aaaa', 'bbbb', 'AAAAA', 'c', 'd', 'e', 'f']
 const intersectionArray = ['BB', 'AAAAA']
 
-const w1 = new Words({ wordsMap: wm1 })
-const w2 = new Words({ wordsMap: wm2 })
+const w1 = new WordsManager({ wordsMap: wm1 })
+const w2 = new WordsManager({ wordsMap: wm2 })
 
-const w1Array = w1.getAllWords()
-const w2Array = w2.getAllWords()
+const w1Array = w1.getAllNames()
+const w2Array = w2.getAllNames()
 
 describe('Set Management Tests', () => {
   test('getAllSets returns all sets', () => {})
@@ -57,12 +53,12 @@ describe('Set Management Tests', () => {
 describe('Word Management Tests', () => {
   test('getAllWords returns flatten array of all words', () => {
     // Core function
-    expect(getAllWords(wm1)).toEqual(expect.arrayContaining(wm1Array))
-    expect(getAllWords(wm2)).toEqual(expect.arrayContaining(wm2Array))
+    expect(getAllNames(wm1)).toEqual(expect.arrayContaining(wm1Array))
+    expect(getAllNames(wm2)).toEqual(expect.arrayContaining(wm2Array))
 
     // Object method
-    expect(w1.getAllWords()).toEqual(expect.arrayContaining(wm1Array))
-    expect(w2.getAllWords()).toEqual(expect.arrayContaining(wm2Array))
+    expect(w1.getAllNames()).toEqual(expect.arrayContaining(wm1Array))
+    expect(w2.getAllNames()).toEqual(expect.arrayContaining(wm2Array))
   })
 })
 
@@ -89,22 +85,22 @@ describe('Arrays operations tests', () => {
     expect(w2.getIntersection([])).toEqual([])
 
     // Static Method
-    expect(Words.getIntersection(w1Array, w2Array)).toEqual(
-      expect.arrayContaining(intersectionArray),
+    expect(WordsManager.getIntersection(w1Array, w2Array)).toEqual(
+      expect.arrayContaining(intersectionArray)
     )
-    expect(Words.getIntersection(w2Array, w1Array)).toEqual(
-      expect.arrayContaining(intersectionArray),
+    expect(WordsManager.getIntersection(w2Array, w1Array)).toEqual(
+      expect.arrayContaining(intersectionArray)
     )
 
-    expect(Words.getIntersection(w1Array, [])).toEqual([])
-    expect(Words.getIntersection(w2Array, [])).toEqual([])
-    expect(Words.getIntersection([], w1Array)).toEqual([])
-    expect(Words.getIntersection([], w2Array)).toEqual([])
+    expect(WordsManager.getIntersection(w1Array, [])).toEqual([])
+    expect(WordsManager.getIntersection(w2Array, [])).toEqual([])
+    expect(WordsManager.getIntersection([], w1Array)).toEqual([])
+    expect(WordsManager.getIntersection([], w2Array)).toEqual([])
 
-    expect(Words.getIntersection(w1Array, w1Array)).toEqual(expect.arrayContaining(w1Array))
-    expect(Words.getIntersection(w2Array, w2Array)).toEqual(expect.arrayContaining(w2Array))
+    expect(WordsManager.getIntersection(w1Array, w1Array)).toEqual(expect.arrayContaining(w1Array))
+    expect(WordsManager.getIntersection(w2Array, w2Array)).toEqual(expect.arrayContaining(w2Array))
 
-    expect(Words.getIntersection([], [])).toEqual([])
+    expect(WordsManager.getIntersection([], [])).toEqual([])
   })
 
   test('getUnion returns correct union of two arrays', () => {
@@ -134,17 +130,17 @@ describe('Arrays operations tests', () => {
     expect(w2.getUnion([])).toEqual(expect.arrayContaining(w2Array))
 
     // Static Method
-    expect(Words.getUnion(w1Array, w2Array)).toEqual(expect.arrayContaining(unionArray))
-    expect(Words.getUnion(w2Array, w1Array)).toEqual(expect.arrayContaining(unionArray))
+    expect(WordsManager.getUnion(w1Array, w2Array)).toEqual(expect.arrayContaining(unionArray))
+    expect(WordsManager.getUnion(w2Array, w1Array)).toEqual(expect.arrayContaining(unionArray))
 
-    expect(Words.getUnion(w1Array, w1Array)).toEqual(expect.arrayContaining(w1Array))
-    expect(Words.getUnion(w2Array, w2Array)).toEqual(expect.arrayContaining(w2Array))
+    expect(WordsManager.getUnion(w1Array, w1Array)).toEqual(expect.arrayContaining(w1Array))
+    expect(WordsManager.getUnion(w2Array, w2Array)).toEqual(expect.arrayContaining(w2Array))
 
-    expect(Words.getUnion(w1Array, [])).toEqual(expect.arrayContaining(w1Array))
-    expect(Words.getUnion(w2Array, [])).toEqual(expect.arrayContaining(w2Array))
-    expect(Words.getUnion([], w1Array)).toEqual(expect.arrayContaining(w1Array))
-    expect(Words.getUnion([], w2Array)).toEqual(expect.arrayContaining(w2Array))
+    expect(WordsManager.getUnion(w1Array, [])).toEqual(expect.arrayContaining(w1Array))
+    expect(WordsManager.getUnion(w2Array, [])).toEqual(expect.arrayContaining(w2Array))
+    expect(WordsManager.getUnion([], w1Array)).toEqual(expect.arrayContaining(w1Array))
+    expect(WordsManager.getUnion([], w2Array)).toEqual(expect.arrayContaining(w2Array))
 
-    expect(Words.getUnion([], [])).toEqual(expect.arrayContaining([]))
+    expect(WordsManager.getUnion([], [])).toEqual(expect.arrayContaining([]))
   })
 })
