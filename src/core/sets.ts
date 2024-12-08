de09f5cd4e551f3b3687285da1a3788bba161cde
Alexander Larin <doremilarin@gmail.com>
2024-12-08 13:38:42 +0300

    feat: add getAllSets function

--- src/core/sets.ts
+++ src/core/sets.ts
@@ -1,21 +1,5 @@
-export function getAllSets() {}
+import { Word, WordsMap } from "@alex-lar/words-shared-types";
 
-export function getSetSize(key: number) {}
-
-export function getSize() {}
-
-export function containsSet(key: number) {
-  /* Returns true if a set is present in the words object, false otherwise. */
-}
-
-export function getRandomSet() {
-  /* Returns a random set from the words object. */
-}
-
-export function getSet(key: number) {
-  /* Returns the set at a specific index in the words object. */
-}
-
-export function removeSet(setKey: number) {
-  /* Removes a set from the words object. */
+export function getAllSets(wordsMap: WordsMap): Word[][]  {
+  return Object.values(wordsMap);
 }
