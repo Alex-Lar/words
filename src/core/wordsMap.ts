52085b1f505f5065412e23dad925b3566e1867dc
Alexander Larin <doremilarin@gmail.com>
2024-12-11 19:31:09 +0300

    chore: remove wordsMap file

--- src/core/wordsMap.ts
+++
@@ -1,6 +0,0 @@
-export function registerWordMap() {
-  /* Register external WordMap object */
-}
-export function sortWordsByType() {
-  /* returns a new word map in which words are grouped by their type */
-}
