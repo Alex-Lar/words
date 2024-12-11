236e60b39513a39a3b1d186f2126d409f750e472
Alexander Larin <doremilarin@gmail.com>
2024-12-11 19:26:20 +0300

    feat: add getSet function

--- src/core/sets.ts
+++ src/core/sets.ts
@@ -3,3 +3,7 @@ import { Word, WordsMap } from "@alex-lar/words-shared-types";
 export function getAllSets(wordsMap: WordsMap): Word[][]  {
   return Object.values(wordsMap);
 }
+
+export function getSet(wordsMap: WordsMap, key: number): Word[] {
+  return wordsMap[key];
+}
